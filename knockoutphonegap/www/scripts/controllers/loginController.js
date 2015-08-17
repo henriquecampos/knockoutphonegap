410d8d19f042e963cb21cec7440fde7e1ef32822
Henrique Campos <henrique.campos@outlook.com.br>
2015-08-17 18:27:15 -0300

    Login controller

---
+++ knockoutphonegap/www/scripts/controllers/loginController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+    angular.module("AngularAuthApp.controllers").controller('LoginController', ['authService', '$location', LoginController]);
+
+    /**
+     * Controller for the login.
+     * 
+     * @param {!angular.Service} storage
+     * @constructor
+     * @export
+     */
+    function LoginController(authService, $location) {
+        this.authService = authService;
+        this.location = $location;
+        this.dados = {
+            Email: "admin@admin.com",
+            Senha: "123456"
+        }
+    }
+
+    LoginController.prototype.entrar = function () {
+        var _this = this;
+        this.authService.entrar(this.dados).then(function (response) {
+            _this.location.path('/contratos');
+        }, function (rejection) {
+            alert(rejection.status);
+        });
+    };
+})();
